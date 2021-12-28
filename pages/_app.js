c919541bd953544022d5d6ce5d382007ccf9e308
Kaj Berg <kb223aw@student.lnu.se>
2021-12-28 10:34:39 +0100

    first

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer>
+        <p>Kossans footer</p>
+      </footer>
+    </>
+  );
 }
 
 export default MyApp
